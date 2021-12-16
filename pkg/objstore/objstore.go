d0df644310f33779b7f32a1a7f285e5358c4a004
Ergin Babani <ergin@autonomic.ai>
2021-12-16 13:43:48 -0500

    Allow prefix for the bucket viewer

--- pkg/objstore/objstore.go
+++ pkg/objstore/objstore.go
@@ -8,6 +8,7 @@ import (
 	"context"
 	"io"
 	"os"
+	"path"
 	"path/filepath"
 	"strings"
 	"time"
@@ -432,6 +433,90 @@ func (b *metricBucket) Close() error {
 
 func (b *metricBucket) Name() string {
 	return b.bkt.Name()
+}
+
+type prefixedBucket struct {
+	bkt    Bucket
+	prefix string
+}
+
+// NewPrefixedBucket returns a new prefixedBucket.
+func NewPrefixedBucket(bkt Bucket, prefix string) *prefixedBucket {
+	return &prefixedBucket{
+		bkt:    bkt,
+		prefix: prefix,
+	}
+}
+
+func (pbkt *prefixedBucket) nameWithPrefix(name string) string {
+	return path.Join(pbkt.prefix, name)
+}
+
+func (pbkt *prefixedBucket) WithExpectedErrs(expectedFunc IsOpFailureExpectedFunc) Bucket {
+	if ib, ok := pbkt.bkt.(InstrumentedBucket); ok {
+		return &prefixedBucket{
+			bkt:    ib.WithExpectedErrs(expectedFunc),
+			prefix: pbkt.prefix,
+		}
+	}
+	return pbkt
+}
+
+func (pbkt *prefixedBucket) ReaderWithExpectedErrs(expectedFunc IsOpFailureExpectedFunc) BucketReader {
+	return pbkt.WithExpectedErrs(expectedFunc)
+}
+
+func (pbkt *prefixedBucket) Name() string {
+	return pbkt.bkt.Name()
+}
+
+func (pbkt *prefixedBucket) Upload(ctx context.Context, name string, r io.Reader) (err error) {
+	pname := pbkt.nameWithPrefix(name)
+	err = pbkt.bkt.Upload(ctx, pname, r)
+	return
+}
+
+func (pbkt *prefixedBucket) Delete(ctx context.Context, name string) (err error) {
+	pname := pbkt.nameWithPrefix(name)
+	err = pbkt.bkt.Delete(ctx, pname)
+	return
+}
+
+func (pbkt *prefixedBucket) Close() error {
+	return pbkt.bkt.Close()
+}
+func (pbkt *prefixedBucket) Iter(ctx context.Context, dir string, f func(string) error) error {
+	pdir := pbkt.nameWithPrefix(dir)
+	if pbkt.prefix != "" {
+		return pbkt.bkt.Iter(ctx, pdir, func(s string) error {
+			return f(strings.Join(strings.Split(s, pbkt.prefix)[1:], "/"))
+		})
+	}
+	return pbkt.bkt.Iter(ctx, pdir, f)
+}
+
+func (pbkt *prefixedBucket) Get(ctx context.Context, name string) (io.ReadCloser, error) {
+	pname := pbkt.nameWithPrefix(name)
+	return pbkt.bkt.Get(ctx, pname)
+}
+
+func (pbkt *prefixedBucket) GetRange(ctx context.Context, name string, off, length int64) (io.ReadCloser, error) {
+	pname := pbkt.nameWithPrefix(name)
+	return pbkt.bkt.GetRange(ctx, pname, off, length)
+}
+
+func (pbkt *prefixedBucket) Exists(ctx context.Context, name string) (bool, error) {
+	pname := pbkt.nameWithPrefix(name)
+	return pbkt.bkt.Exists(ctx, pname)
+}
+
+func (pbkt *prefixedBucket) IsObjNotFoundErr(err error) bool {
+	return pbkt.bkt.IsObjNotFoundErr(err)
+}
+
+func (pbkt *prefixedBucket) Attributes(ctx context.Context, name string) (ObjectAttributes, error) {
+	pname := pbkt.nameWithPrefix(name)
+	return pbkt.bkt.Attributes(ctx, pname)
 }
 
 type timingReadCloser struct {
